4b5a3454e5d1b93492b352b99ae5a71d6c14b9a2
Md. Rashedul Hasan <76987245+mdrashedulhasan1@users.noreply.github.com>
2022-06-01 09:03:31 +0600

    Add files via upload
    JS-Six-Core-Parts

---
+++ index.js
@@ -0,0 +1,62 @@
+//1.Variable Declare
+// var mangoPrice = 10;
+// console.log(mangoPrice);
+
+//2.if, else if, else
+// var mangoPrice = 10;
+// if(mangoPrice>10){
+
+// }
+// else if(mangoPrice>=10){
+
+// }
+// else if(mangoPrice<10){
+
+// }
+// else if(mangoPrice<=10){
+
+// }
+// else if(mangoPrice==10){
+
+// }
+// else if(mangoPrice!=10){
+
+// }
+// else{
+
+// }
+
+//3.Array
+// var ages = [12, 28, 34, 45, 56, 23, 34, 45];
+// ages.push(47);
+// ages.pop();
+// if(ages.indexOf(28) != -1){
+//     console.log('Number Found!');
+// }
+// if(ages.includes(45)){
+//     console.log('Number Found!')
+// }
+// console.log(ages);
+
+//4.loop, while
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+// let i=1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+//5.Function
+// function addName(first, last){
+//     return first+" "+last;
+// }
+// const fullName = addName('Rashedul', 'Hasan');
+// console.log(fullName);
+
+//Object
+var webDesign = {id:1, courseName:'Web Design', price:'$20'};
+console.log(webDesign.id, webDesign.courseName, webDesign.price);
+
